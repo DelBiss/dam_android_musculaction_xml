85af276bd643e72d65788324a9ae38726a13a2ac
Philippe Allard-Rousse <philrousse@gmail.com>
2022-06-30 12:56:21 -0400

    Data:Entity: Reorder table Entity into used order

--- data/entity/src/main/java/ca/philrousse/android02/musculaction/data/entity/Table.kt
+++ data/entity/src/main/java/ca/philrousse/android02/musculaction/data/entity/Table.kt
@@ -4,46 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Exercise(
-    override var name:String="N/D",
-    override var description:String="N/D",
-    override var imageID:String="N/D",
-    var parentId:Long?=null,
-    @PrimaryKey(autoGenerate = true)
-    override val id:Long?=null
-): DescriptiveCard {
-    override fun toString(): String {
-        return "Exercise(id=$id,name=$name,imageID=$imageID)"
-    }
-}
-
-
-@Entity
-data class ExerciseDetailVideo (
-    var videoUrl:String="N/D",
-    var parentId:Long?=null,
-    @PrimaryKey(autoGenerate = true)
-    val id:Long?=null
-){
-    override fun toString(): String {
-        return "ExerciseDetailVideo(id=$id,videoUrl=$videoUrl)"
-    }
-}
-
-@Entity
-data class ExerciseDetail(
-    var name:String="N/D",
-    var description:String="N/D",
-    var parentId:Long?=null,
-    @PrimaryKey(autoGenerate = true)
-    val id:Long?=null
-){
-    override fun toString(): String {
-        return "ExerciseDetail(id=$id,name=$name)"
-    }
-}
-
-@Entity
 data class ExercisesCategory(
     override var name:String="N/D",
     override var description:String="N/D",
@@ -66,5 +26,44 @@ data class ExercisesSubcategory(
 ){
     override fun toString(): String {
         return "ExercisesSubcategory(id=$id,name=$name)"
+    }
+}
+
+@Entity
+data class Exercise(
+    override var name:String="N/D",
+    override var description:String="N/D",
+    override var imageID:String="N/D",
+    var parentId:Long?=null,
+    @PrimaryKey(autoGenerate = true)
+    override val id:Long?=null
+): DescriptiveCard {
+    override fun toString(): String {
+        return "Exercise(id=$id,name=$name,imageID=$imageID)"
+    }
+}
+
+@Entity
+data class ExerciseDetail(
+    var name:String="N/D",
+    var description:String="N/D",
+    var parentId:Long?=null,
+    @PrimaryKey(autoGenerate = true)
+    val id:Long?=null
+){
+    override fun toString(): String {
+        return "ExerciseDetail(id=$id,name=$name)"
+    }
+}
+
+@Entity
+data class ExerciseDetailVideo (
+    var videoUrl:String="N/D",
+    var parentId:Long?=null,
+    @PrimaryKey(autoGenerate = true)
+    val id:Long?=null
+){
+    override fun toString(): String {
+        return "ExerciseDetailVideo(id=$id,videoUrl=$videoUrl)"
     }
 }
